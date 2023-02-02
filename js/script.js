7b3f5f718369bcbef27706e53e063453c73daa23
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-02-02 16:48:14 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//1 stampare tutti i numeri da 1 a 100
+
+for ( let i = 1 ; i <= 100 ; i++){
+    console.log(i);
+
+//2 al posto di stampare i numeri multipli di 3, 5 e 15 scrivere corrispettivamente Fizz Buzz e FizzBuzz
+
+};

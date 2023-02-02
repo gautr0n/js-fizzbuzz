fb350c24cc85f5ccad925e8c9bdba273d7b83c45
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-02-02 16:54:19 +0100

    completamento fizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 //1 stampare tutti i numeri da 1 a 100
-
-for ( let i = 1 ; i <= 100 ; i++){
-    console.log(i);
-
 //2 al posto di stampare i numeri multipli di 3, 5 e 15 scrivere corrispettivamente Fizz Buzz e FizzBuzz
-
+for ( let i = 1 ; i <= 100 ; i++){
+    if(i % 15 === 0){
+        console.log(`FizzBuzz`);
+    } else if(i % 3 === 0){
+        console.log(`Fizz`);
+    } else if( i % 5 === 0){
+        console.log(`Buzz`);
+    } else
+    console.log(i);
 };
